617fea08556b6781cb95305f70c04d01dce0e1eb
rlram <rlramlakhan02@gmail.com>
2024-11-15 11:18:08 +0530

    add sign in layout

--- app/src/main/java/com/example/uptask/SignInActivity.kt
+++ app/src/main/java/com/example/uptask/SignInActivity.kt
@@ -1,30 +1,20 @@
 package com.example.uptask
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-@SuppressLint("CustomSplashScreen")
-class SplashActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_sign_in)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(applicationContext, SignInActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 2000)
     }
 }
